9c86d4c66902cc1ff7f3f3bb20de47b56bd7962f
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-15 08:02:34 +0200

    Vite env declarations.

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+interface ImportMetaEnv {
+    readonly MODE: string;
+    readonly BASE_URL: string;
+    readonly PROD: boolean;
+    readonly DEV: boolean;
+    readonly SSR: boolean;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
