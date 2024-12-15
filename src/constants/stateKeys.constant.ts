bd85b66caf9c66a2db7ba71f427ae2d0706093b9
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-15 09:33:10 +0200

    Constants.

---
+++ src/constants/stateKeys.constant.ts
@@ -0,0 +1,14 @@
+const StateKeys = {
+    // Events.
+    // // Navigation.
+    navigate: 'navigate',
+    contentReady: 'contentReady',
+    footerUpdate: 'footerUpdate',
+    // // Dialogs.
+    openModal: 'openModal',
+    openDialog: 'openDialog',
+    closeDialog: 'closeDialog',
+} as const;
+
+
+export { StateKeys };
