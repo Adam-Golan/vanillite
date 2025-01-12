ba04df3e619974d1ca360230708079c9c42b3919
Adam-Golan <slowmoon.adam@gmail.com>
2025-01-12 16:17:10 +0200

    Basis -> Upgrading in favor of memory efficiency.

--- src/decorators/base.ts
+++ src/decorators/base.ts
@@ -20,45 +20,35 @@ export abstract class Basis<IConfig> extends HTMLElement {
      * @param tag The tag name of the element to create, e.g. "div", "span", etc.
      * @returns The created element.
      */
-    protected cElem<K extends keyof HTMLElementTagNameMap>(tag: K): HTMLElementTagNameMap[K] {
-        return document.createElement<K>(tag);
-    }
+    protected cElem = document.createElement.bind(document);
 
     /**
      * Get an element by its id.
      * @param id The id of the element to retrieve.
      * @returns The element with the given id, or null if it doesn't exist.
      */
-    protected idElem<K extends keyof HTMLElementTagNameMap>(id: K): HTMLElementTagNameMap[K] | null {
-        return this.querySelector(`#${id}`);
-    }
+    protected idElem = <K extends keyof HTMLElementTagNameMap>(id: string): HTMLElementTagNameMap[K] | null => this.querySelector(`#${id}`);
 
     /**
      * Get elements by their "name" attribute.
      * @param name The value of the "name" attribute to search for.
      * @returns A list of elements with a "name" attribute equal to the given value.
      */
-    protected nmElem<K extends string>(name: K): NodeListOf<HTMLInputElement> {
-        return this.querySelectorAll(`[name="${name}"]`);
-    }
+    protected nmElem = (name: string): NodeListOf<HTMLElement> => this.querySelectorAll(`[name="${name}"]`);
 
     /**
      * Get elements by their "class" attribute.
      * @param className The value of the "class" attribute to search for.
      * @returns A list of elements with a "class" attribute equal to the given value.
      */
-    protected clsElem<K extends string>(className: K): NodeListOf<HTMLElement & { className: K }> {
-        return this.querySelectorAll(`.${className}`);
-    }
+    protected clsElem = this.getElementsByClassName;
 
     /**
      * Get elements by their tag name.
      * @param tag The tag name of the elements to retrieve, e.g. "div", "span", etc.
      * @returns A collection of elements with the specified tag name.
      */
-    protected tagElem<K extends keyof HTMLElementTagNameMap>(tag: K): HTMLCollectionOf<HTMLElementTagNameMap[K]> {
-        return this.getElementsByTagName<K>(tag);
-    }
+    protected tagElem = this.getElementsByTagName;
 
 
     /**
